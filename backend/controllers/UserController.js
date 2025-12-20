e6093a575126d496f2d48224bf881e679128463e
codewitharfath <mdarfath239@gmail.com>
2025-12-20 10:28:22 +0530

    updated everything is working

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -3,6 +3,13 @@ import User from '../models/UserModel.js'
 import fs from 'fs'
 import axios from 'axios'
 import FormData from 'form-data'
+import razorpay from 'razorpay'
+import transactionModel from '../models/transactionModel.js'
+
+const razorpayInstance = new razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
+})
 
 // API Controller Function to Manage Clerk User with database
 // http://localhost:4000/api/user/webhooks
@@ -149,4 +156,104 @@ const removeImageBg = async (req, res) => {
     }
 }
 
-export { clerkWebhooks, credits, removeImageBg }
+// API to make payment for credits
+const paymentRazorpay = async (req, res) => {
+    try {
+
+        const { clerkId, planId } = req.body
+        const userData = await User.findOne({ clerkId })
+
+        if (!userData || !planId) {
+            return res.json({ success: false, message: 'Invalid Credentials' })
+        }
+
+        let credits, plan, amount, date
+
+        switch (planId) {
+            case 'Basic':
+                plan = 'Basic'
+                credits = 100
+                amount = 10
+                break;
+            case 'Advanced':
+                plan = 'Advanced'
+                credits = 500
+                amount = 50
+                break;
+            case 'Business':
+                plan = 'Business'
+                credits = 5000
+                amount = 250
+                break;
+            default:
+                break;
+        }
+
+        date = Date.now()
+
+        // Creating Transaction Data
+        const transactionData = await transactionModel.create({
+            clerkId,
+            plan,
+            amount,
+            credits,
+            date
+        })
+
+        // Creating Options for Razorpay Order
+        const options = {
+            amount: amount * 100,
+            currency: process.env.CURRENCY,
+            receipt: transactionData._id.toString()
+        }
+
+        // Creating Razorpay Order
+        const order = await razorpayInstance.orders.create(options)
+
+        res.json({ success: true, order })
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// API Controller function to verify razorpay payment
+const verifyRazorpay = async (req, res) => {
+    try {
+
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body
+
+        const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id)
+
+        if (orderInfo.status === 'paid') {
+            const transactionData = await transactionModel.findById(orderInfo.receipt)
+
+            if (!transactionData) {
+                return res.json({ success: false, message: 'Transaction not found' })
+            }
+
+            if (transactionData.payment) {
+                return res.json({ success: false, message: 'Payment Failed' })
+            }
+
+            // Adding Credits in user data
+            const userData = await User.findOne({ clerkId: transactionData.clerkId })
+            const creditBalance = userData.creditBalance + transactionData.credits
+            await User.findByIdAndUpdate(userData._id, { creditBalance })
+
+            // Making payment true in transaction data
+            await transactionModel.findByIdAndUpdate(transactionData._id, { payment: true })
+
+            res.json({ success: true, message: "Credits Added" })
+        } else {
+            res.json({ success: false, message: 'Payment Failed' })
+        }
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+export { clerkWebhooks, credits, removeImageBg, paymentRazorpay, verifyRazorpay }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { clerkWebhooks, credits, removeImageBg } from '../controllers/UserController.js'
+import { clerkWebhooks, credits, removeImageBg, paymentRazorpay, verifyRazorpay } from '../controllers/UserController.js'
 import upload from '../middleware/multer.js'
 import authUser from '../middleware/auth.js'
 
@@ -8,5 +8,7 @@ const userRouter = express.Router()
 userRouter.post('/webhooks', clerkWebhooks)
 userRouter.get('/credits', authUser, credits)
 userRouter.post('/remove-bg', upload.single('image'), authUser, removeImageBg)
+userRouter.post('/pay-razorpay', authUser, paymentRazorpay)
+userRouter.post('/verify-razorpay', verifyRazorpay)
 
 export default userRouter
